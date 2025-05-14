6f11ef496cd7039275fd5d2a65a01bfe3ac29528
Yeju-Kang <bbogaeme@github.com>
2025-05-14 23:56:40 +0900

    fix : url 수정

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://43.200.172.192:8080",
+  baseURL: "http://localhost:8080",
   headers: {
     "Content-Type": "application/json",
   },
